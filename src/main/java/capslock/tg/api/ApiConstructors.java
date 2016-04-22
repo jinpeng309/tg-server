54c366aa14d5ab56dfff0117fef5795e7f51f6e3
alvin <alvin@liao.com>
2016-04-22 13:48:33 +0800

    add ResPq

--- src/main/java/capslock/tg/api/ApiConstructors.java
+++ src/main/java/capslock/tg/api/ApiConstructors.java
@@ -5,5 +5,6 @@ package capslock.tg.api;
  */
 public final class ApiConstructors {
     public static final int REQ_PQ = 0x60469778;
+    public static final int RES_PQ = 0x05162463;
 
 }
--- src/main/java/capslock/tg/api/object/ResPqObject.java
+++ src/main/java/capslock/tg/api/object/ResPqObject.java
@@ -5,14 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
- * Created by capslock.
+ * Created by alvin.
  */
 @Data
 @AllArgsConstructor
-public final class ReqPqObject extends TLObject {
-    private final static int CONSTRUCTOR_ID = ApiConstructors.REQ_PQ;
-
-    private final byte[] nonce;
+public final class ResPqObject extends TLObject {
+    private final static int CONSTRUCTOR_ID = ApiConstructors.RES_PQ;
 
     @Override
     public int getConstructId() {
